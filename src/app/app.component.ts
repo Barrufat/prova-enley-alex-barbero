0682e39b759b95aa334479216882db77f6419b78
Alex Barbero <barrufat@hotmail.com>
2024-09-27 14:21:34 +0200

    Add character card & list styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CharacterService } from '../services/character/character.service';
 import { CharactersPageComponent } from '../pages/characters-page/characters-page.component';
+import { NavbarComponent } from '../components/navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CharactersPageComponent],
+  imports: [RouterOutlet, CharactersPageComponent, NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
